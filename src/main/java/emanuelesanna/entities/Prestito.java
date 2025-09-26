fe98721c2d4194366ae5485d5f87b0379c2d45c2
EmanSan9 <sanna950@gmail.com>
2025-09-26 12:21:06 +0200

    V4

--- src/main/java/emanuelesanna/entities/Prestito.java
+++ src/main/java/emanuelesanna/entities/Prestito.java
@@ -2,16 +2,93 @@ package emanuelesanna.entities;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
 public class Prestito {
     @Id
     @GeneratedValue
+    @Column(name = "id_prestito")
     private UUID idPrestito;
-
     @ManyToOne
-    @JoinColumn(name = "utente_id", referencedColumnName = "numeroDiTessera");
+    @JoinColumn(name = "utente_id", referencedColumnName = "numeroDiTessera")
     private Utente utente;
-    @OneToMany
+    @ManyToOne
+    @JoinColumn(name = "elemento_prestato_id", referencedColumnName = "codiceIsbn")
+    private ElementoMultimediale elementoPrestato;
+    @Column(name = "data_inizio_prestito", nullable = false)
+    private LocalDate dataInizioPrestito;
+    @Column(name = "data_restituzione_prevista", nullable = false)
+    private LocalDate dataRestituzionePrevista;
+    @Column(name = "data_restituzione_effettiva")
+    private LocalDate dataRestituzioneEffettiva;
+
+    public Prestito() {
+    }
+
+    public Prestito(Utente utente, ElementoMultimediale elementoPrestato, LocalDate dataInizioPrestito, LocalDate dataRestituzionePrevista, LocalDate dataRestituzioneEffettiva) {
+        this.utente = utente;
+        this.elementoPrestato = elementoPrestato;
+        this.dataInizioPrestito = dataInizioPrestito;
+        this.dataRestituzionePrevista = dataRestituzionePrevista;
+        this.dataRestituzioneEffettiva = dataRestituzioneEffettiva;
+    }
+
+    public UUID getIdPrestito() {
+        return idPrestito;
+    }
+
+    public Utente getUtente() {
+        return utente;
+    }
+
+    public void setUtente(Utente utente) {
+        this.utente = utente;
+    }
+
+    public ElementoMultimediale getElementoPrestato() {
+        return elementoPrestato;
+    }
+
+    public void setElementoPrestato(ElementoMultimediale elementoPrestato) {
+        this.elementoPrestato = elementoPrestato;
+    }
+
+    public LocalDate getDataInizioPrestito() {
+        return dataInizioPrestito;
+    }
+
+    public void setDataInizioPrestito(LocalDate dataInizioPrestito) {
+        this.dataInizioPrestito = dataInizioPrestito;
+        this.dataRestituzionePrevista = dataInizioPrestito.plusDays(30);
+    }
+
+    public LocalDate getDataRestituzionePrevista() {
+        return dataRestituzionePrevista;
+    }
+
+    public void setDataRestituzionePrevista(LocalDate dataRestituzionePrevista) {
+        this.dataRestituzionePrevista = dataRestituzionePrevista;
+    }
+
+    public LocalDate getDataRestituzioneEffettiva() {
+        return dataRestituzioneEffettiva;
+    }
+
+    public void setDataRestituzioneEffettiva(LocalDate dataRestituzioneEffettiva) {
+        this.dataRestituzioneEffettiva = dataRestituzioneEffettiva;
+    }
+
+    @Override
+    public String toString() {
+        return "Prestito{" +
+                "idPrestito=" + idPrestito +
+                ", utente=" + utente +
+                ", elementoPrestato=" + elementoPrestato +
+                ", dataInizioPrestito=" + dataInizioPrestito +
+                ", dataRestituzionePrevista=" + dataRestituzionePrevista +
+                ", dataRestituzioneEffettiva=" + dataRestituzioneEffettiva +
+                '}';
+    }
 }
